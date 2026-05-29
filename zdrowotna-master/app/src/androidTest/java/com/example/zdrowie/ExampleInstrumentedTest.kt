24e73fcd4ca6ce3670d2f97e94278d23bca01b71
NMPNNXD <148040146+NMPNNXD@users.noreply.github.com>
2026-05-29 23:01:04 +0200

    Update package name in ExampleInstrumentedTest

--- zdrowotna-master/app/src/androidTest/java/com/example/zdrowie/ExampleInstrumentedTest.kt
+++ zdrowotna-master/app/src/androidTest/java/com/example/zdrowie/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.wodaisamopoczucie
+package com.example.zdrowie
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.wodaisamopoczucie", appContext.packageName)
+        assertEquals("com.example.zdrowie", appContext.packageName)
     }
 }
